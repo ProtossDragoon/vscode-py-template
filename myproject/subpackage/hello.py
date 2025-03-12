20d7da872da3c4e436732bbdd56b973c0e118437
Janghoo Lee <46595649+ProtossDragoon@users.noreply.github.com>
2025-03-12 18:34:42 +0900

    Update hello.py

--- myproject/subpackage/hello.py
+++ myproject/subpackage/hello.py
@@ -1,9 +1,15 @@
+import logging
+
+
 print(__file__)
 
 
 def hello_world():
-    print('hello world!')
+    logger = logging.getLogger('myproject')
+    logger.info('hello world!')
 
 
 if __name__ == '__main__':
-    print('hello!')
+    from myproject.log_utils import setup_logging
+    setup_logging()
+    hello_world()
